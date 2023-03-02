d347e0e67379306464551b26a15f9b2ece7b07e9
MissBlumarine <miss_blumarine01@mail.ru>
2023-03-02 05:35:03 +0300

    commit_1

--- sort.py
+++ sort.py
@@ -57,6 +57,52 @@ def insertion_sort(nums):
         nums[j + 1] = item_to_insert
 
 
-random_list_of_nums = [1, 2, 0, 3, 5, 6, 8, 9, 7, 11, 34, 12, 13, 15]
-insertion_sort(random_list_of_nums)
-print(random_list_of_nums)
+# random_list_of_nums = [1, 2, 0, 3, 5, 6, 8, 9, 7, 11, 34, 12, 13, 15]
+# insertion_sort(random_list_of_nums)
+# print(random_list_of_nums)
+
+
+# Сортировка кучей (пирамидальная)
+
+def heapify (nums, heap_size, root_index):
+    # создаем вспомогательную функцию для реализации нахождения вершины кучи
+    # индекс наибольшего элемента считаем корневым индексом
+    largest = root_index
+    left_child = (2 * root_index) + 1
+    right_child = (2 * root_index) + 2
+
+    # если левый потомок корня - допустимый индекс, а элемент больше
+    # чем текущий наибольший --> обновляем наибольший элемент
+    if left_child < heap_size and nums[left_child] > nums[largest]:
+        largest = left_child
+
+    # аналогично для правого потока
+    if right_child < heap_size and nums[right_child] > nums[largest]:
+        largest = right_child
+
+    # если наибольший элемент больше не корневой, они меняются местами
+    if root_index != largest:
+        nums[root_index], nums[largest] = nums[largest], nums[root_index]
+
+        # теперь проверяем новый корневой элемент, чтобы убедиться, что он наибольший
+        heapify(nums, heap_size, largest)
+
+def heap_sort(nums):
+    n = len(nums)
+
+    # - создаем MAX HEAP из списка
+    # - второй аргумент означает остановку алгоритма перед элементов -1, т.е. перед первым элементом списка
+    # - третий аргумент означает повторный проход по списку в обратном направлении,
+    #   уменьшая счетчик i на 1
+    for i in range(n, -1, -1):
+        heapify(nums, n, i)
+
+    # переменная корень MAX HEAP в конец списка
+    for i in range(n - 1, 0, -1):
+        nums[i], nums[0] = nums[0], nums[i]
+        heapify(nums, i, 0)
+
+
+# random_list_of_nums = [1, 2, 0, 3, 5, 6, 8, 9, 7, 11, 34, 12, 13, 15]
+# heap_sort(random_list_of_nums)
+# print(random_list_of_nums)
